1e7d1fe5d2730fc58270a0ce3616959769715d05
ryanma3003 <ryan.martin3003@gmail.com>
2022-11-29 11:34:44 +0700

    featured: updated urls static

--- csirt/urls.py
+++ csirt/urls.py
@@ -55,3 +55,4 @@ handler500 = 'csirt.views.handler500'
 
 # if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
